a39ac7756c83e90606074f0ec46f9ea30870ee62
James Lemire <webdev.jameslemire@gmail.com>
2017-06-07 12:02:15 -0600

    JL assessment-1 - Remove unnecessary nesting.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Tasks from './Tasks/Tasks';
+import AddTask from './AddTask/AddTask';
+import List from './List/List.js';
 
 class App extends Component {
   render() {
     return (
       <div id="App__container">
-        <Tasks />
+        <div id="Tasks__container">
+          <div id="Tasks__header">
+            <span> To Do </span>
+          </div>
+
+          <AddTask />
+          <List />
+        </div>
       </div>
     );
   }
