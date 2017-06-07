0422a2765e41a612c92913f0dac506de64c12bee
James Lemire <webdev.jameslemire@gmail.com>
2017-06-07 13:33:17 -0600

    JL assessment-3 - Complete.

--- src/components/Main/List/Task/Task.js
+++ src/components/Main/List/Task/Task.js
@@ -4,11 +4,10 @@ import './Task.css';
 import { dispatchDeleteTask } from '../../../../services/task_service';
 import { Link } from 'react-router-dom';
 
-export default function Task( { id, title, complete } ) {
-  console.log('IS IT COMPLETED?', complete);
+export default function Task( { id, title, completed } ) {
   return (
     <Link id="Task__link" to={ `details/${id}` }>
-      <div className={"Task__container " + (complete ? 'complete' : '') }>
+      <div className={"Task__container " + (completed ? 'complete' : '') }>
         <span> { title } </span>
         <span id="Task__delete" onClick={ () => dispatchDeleteTask( id ) }> x </span>
       </div>
